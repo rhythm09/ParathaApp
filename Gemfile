e5a325bbad99d4f8666abe92f784d42f180c4ad2
rhjain09 <rhjain@grepruby.io>
2021-03-16 10:52:28 +0530

    Bootsratp

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.0'
-gem 'bootstrap-sass', '~> 3.3.6'  
 
 
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,9 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
   
   def current_user
-    if  @current_user.nil?
-      @current_user = User.find_by(id: session[:current_user_id])
-    else
-      @current_user 
-    end
+    @current_user ||= User.find_by(id: session[:current_user_id])
   end  
  
-
   def logged_in?
     current_user.present?  
   end
