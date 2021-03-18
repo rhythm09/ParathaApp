6d0bc2e5c3e404bd36e6e4772c95bf3a1aad02f6
rhjain09 <79077656+rhjain09@users.noreply.github.com>
2021-03-18 13:16:58 +0530

    User Authentication (#1)
    * User Authentication
    
    * User Authentication Modified
    
    * User Authentication Modified
    
    * User Authentication Modified
    
    * User Authentication Modified
    
    * User Authentication
    
    * User Authentication Final Modified

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  def new; end
+
+  def create
+    @user = User.find_by(email: login_params[:email])
+    if @user&.authenticate(login_params[:password])
+      session[:current_user_id] = @user.id
+      redirect_to root_path 
+    else 
+      flash[:danger] = 'Invalid email and password'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    flash[:notice] = 'Successfully Logged Out'
+    redirect_to root_path
+  end
+
+  private 
+  
+  def login_params
+    params.require(:session).permit(:email, :password)
+  end
+end
