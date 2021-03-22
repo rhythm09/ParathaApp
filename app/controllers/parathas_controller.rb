266cacc63c7ce70551a249f550ac7402040feed0
rhjain09 <rhjain@grepruby.io>
2021-03-22 13:37:20 +0530

    Save

--- app/controllers/parathas_controller.rb
+++ app/controllers/parathas_controller.rb
@@ -1,2 +1,24 @@
 class ParathasController < ApplicationController
+  
+  def new
+    @paratha = Paratha.new
+  end
+
+  def create
+    @paratha = Paratha.new(paratha_params)
+    if @paratha.save
+      flash[:success] = "Paratha Added Successfully"
+      redirect_to root_path
+    else
+      flash.now[:danger] = @paratha.errors.full_messages
+      render :new  
+    end
+  end
+
+  def show
+  end
+
+  def paratha_params
+    params.require(:paratha).permit(:name, :price, :category, :description)
+  end
 end
--- app/models/paratha.rb
+++ app/models/paratha.rb
@@ -1,2 +1,5 @@
 class Paratha < ApplicationRecord
+  validates :name, presence: true
+  validates :price, numericality: true
+  validates :category, presence: true
 end
