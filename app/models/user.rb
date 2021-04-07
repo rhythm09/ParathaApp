e63a2b2c110d81d636fdf3dc39f6fa296e70c8d5
rhjain09 <rhjain@grepruby.io>
2021-04-07 17:55:24 +0530

    Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   has_one_attached :image, dependent: :destroy
   has_one :cart, dependent: :destroy
   has_many :orders, dependent: :destroy
-  # has_secure_password
   validates :password, confirmation: true
   validates :name, presence: true
   validates :email, format: { with: /[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+/ }
